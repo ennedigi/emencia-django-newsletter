b71bdb2ed4e60e7581bc6ddf24d5e73f93e3fb58
Your Name <you@example.com>
2014-09-21 11:41:53 +0200

    update hash library Django 1.5

--- emencia/django/newsletter/utils/tokens.py
+++ emencia/django/newsletter/utils/tokens.py
@@ -13,9 +13,8 @@ class ContactTokenGenerator(object):
 
     def make_token(self, contact):
         """Method for generating the token"""
-        from django.utils.hashcompat import sha_constructor
-
-        token = sha_constructor(settings.SECRET_KEY + unicode(contact.id) +
+        import hashlib
+        token = hashlib.sha1(settings.SECRET_KEY + unicode(contact.id) +
                                 contact.email).hexdigest()[::2]
         return token
 
